8518d3155b47ca10e9b28aa5cd04ac75d8289b58
Sam Millows <sammillows@Sams-MacBook-Air.local>
2024-12-16 15:07:37 +0100

    Improve python script

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,13 +2,23 @@ import json
 
 
 def load_data(file_path):
-    with open(file_path, 'r') as handle:
-        return json.load(handle)
+    try:
+        with open(file_path, 'r') as handle:
+            return json.load(handle)
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path}' was not found.")
+        return None
+    except json.JSONDecodeError:
+        print(f"Error: Failed to decode JSON from the file '{file_path}'.")
+        return None
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return None
 
 
 def serialize_animal(animal):
     """
-    Serialize a single animal object into an HTML string with improved structure.
+    Serialize a single animal object from json into an HTML string on a list.
     """
     output = ""
     output += '<li class="cards__item">\n'
